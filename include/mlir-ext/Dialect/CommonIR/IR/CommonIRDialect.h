bec3ab9432ae76794a8f5264b4c2485f3bd3e495
zhongsanming <zhongsanming@compiler-dev.com>
2026-08-27 14:18:57 +0800

    fix pre-commit checks

--- include/mlir-ext/Dialect/CommonIR/IR/CommonIRDialect.h
+++ include/mlir-ext/Dialect/CommonIR/IR/CommonIRDialect.h
@@ -1,4 +1,4 @@
-//===- CommonIRDialect.h - MLIR CommonIR dialect --------------------*- C++ -*-===//
+//===- CommonIRDialect.h - MLIR CommonIR dialect -------------- -*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
